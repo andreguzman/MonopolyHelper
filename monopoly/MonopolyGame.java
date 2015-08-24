a797a5a559bd9f4ef3781011868593ae643e21ce
andreguzman <an.guzmn@gmail.com>
2015-08-24 16:44:48 -0400

    Implemented commands and added trade functionality to MonoployPlayers.

--- monopoly/MonopolyGame.java
+++ monopoly/MonopolyGame.java
@@ -9,6 +9,7 @@ import java.util.TreeSet;
 public class MonopolyGame {
 	private static final String EXIT_GAME_STRING = "exit";
 	private static final String EXIT_SETUP_STRING = "quit";
+	private static final String BANK_STRING = "bank";
 	private static final Map<String, MonopolyProperty> allProperties = initializeAllProperties();
 
 	public static void main(String[] args){
@@ -28,8 +29,12 @@ public class MonopolyGame {
 			}
 
 			if (!EXIT_SETUP_STRING.equals(name)) {
-				playerMap.put(name, new MonopolyPlayer(name));
-				playerNumber++;
+				if (!BANK_STRING.equals(name)) {
+					playerMap.put(name, new MonopolyPlayer(name));
+					playerNumber++;
+				} else {
+					System.out.println(String.format("Sorry, your name can't be %s.", BANK_STRING));
+				}
 			} else {
 				if (playerMap.size() < 2) {
 					System.out.println("Please enter at least two players.");
@@ -51,13 +56,80 @@ public class MonopolyGame {
 			String[] actionStrings = commandString.split(" ");
 
 			switch (actionStrings[0]) {
-			case "state": displayGameState(playerMap); break;
-			case "exit": break;
-			default: System.out.println("Invalid Command"); continue;
+				case "state": displayGameState(playerMap); break;
+				case "add": addMoneyToPlayerFromBank(actionStrings, playerMap); break;
+				case "passgo": passgoCommand(actionStrings, playerMap); break;
+				case "pay": payCommand(actionStrings, playerMap); break;
+				case "trade": tradeCommand(actionStrings, playerMap); break;
+				case "exit": break;
+				default: System.out.println("Invalid Command"); continue;
 			}
+
+			displayGameState(playerMap);
 		}
 
 		quitGame(in);
+	}
+
+	private static void tradeCommand(String[] actionStrings,
+			Map<String, MonopolyPlayer> playerMap) {
+		if (actionStrings.length != 4) {
+			System.out.println("trade command must have three arguments.");
+			return;
+		}
+
+		tradeMoneyBetweenPlayers(actionStrings[1], actionStrings[2], actionStrings[3], playerMap);
+	}
+
+	private static void payCommand(String[] actionStrings, Map<String, MonopolyPlayer> playerMap) {
+		if (actionStrings.length != 4) {
+			System.out.println("pay command must have three arguments.");
+			return;
+		}
+
+		tradeMoneyBetweenPlayers(actionStrings[2], actionStrings[1], actionStrings[3], playerMap);
+	}
+
+	private static void tradeMoneyBetweenPlayers(String fromPlayer, String toPlayer, String amount,
+			Map<String, MonopolyPlayer> playerMap) {
+		int addCash = 0;
+
+		try {
+			addCash = Integer.parseInt(amount);
+		} catch (NumberFormatException e) {
+			System.out.println(String.format("%s is not a valid amount of money.", addCash));
+			return;
+		}
+
+		if (playerMap.containsKey(toPlayer)) {
+			if (BANK_STRING.equals(fromPlayer)) {
+				playerMap.get(toPlayer).addCash(addCash);
+			} else if (playerMap.containsKey(fromPlayer)) {
+				playerMap.get(fromPlayer).tradeCash(playerMap.get(toPlayer), addCash);
+			} else {
+				System.out.println(String.format("%s is not a player in this game", fromPlayer));
+			}
+		} else {
+			System.out.println(String.format("%s is not a player in this game", toPlayer));
+		}
+	}
+
+	private static void passgoCommand(String[] actionStrings,
+			Map<String, MonopolyPlayer> playerMap) {
+		if (actionStrings.length != 2) {
+			System.out.println("passgo command must have one argument.");
+		}
+
+		tradeMoneyBetweenPlayers(BANK_STRING, actionStrings[1], "200", playerMap);
+	}
+
+	private static void addMoneyToPlayerFromBank(String[] actionStrings,
+			Map<String, MonopolyPlayer> playerMap) {
+		if (actionStrings.length != 3) {
+			System.out.println("add command must have two arguments.");
+		}
+
+		tradeMoneyBetweenPlayers(BANK_STRING, actionStrings[1], actionStrings[2], playerMap);
 	}
 
 	private static void displayGameState(Map<String, MonopolyPlayer> playerMap) {
--- monopoly/MonopolyPlayer.java
+++ monopoly/MonopolyPlayer.java
@@ -29,7 +29,11 @@ public class MonopolyPlayer {
 	public boolean addProperties(MonopolyProperty addProperties) {
 		return properties.add(addProperties);
 	}
-	
+
+	public boolean removeProperty(MonopolyProperty property) {
+		return properties.remove(property);
+	}
+
 	public int addCash(int addAmount) {
 		cash += addAmount;
 		return cash;
@@ -46,5 +50,24 @@ public class MonopolyPlayer {
 	
 	public boolean checkIfPlayerHasProperty(MonopolyProperty checkProperty) {
 		return properties.contains(checkProperty);
+	}
+
+	public boolean tradeCash(MonopolyPlayer other, int amount) {
+		if (amount < 0 || getCash() - amount < 0) {
+			return false;
+		}
+
+		subtractCash(amount);
+		other.addCash(amount);
+		return true;
+	}
+
+	public boolean tradeProperty(MonopolyPlayer other, MonopolyProperty tradeProperty) {
+		if (!checkIfPlayerHasProperty(tradeProperty)
+				|| !other.checkIfPlayerHasProperty(tradeProperty)) {
+			return false;
+		}
+
+		return removeProperty(tradeProperty) && other.addProperty(tradeProperty);
 	}
 }
